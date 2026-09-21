726c04c303a96cc5e2ff87291421805ef1012267
dziedzicdaniel6 <dziedzicdaniel6@gmail.com>
2026-09-21 16:40:01 +0200

    Function declarations

--- LE5_VS/LE5_VS/main.cpp
+++ LE5_VS/LE5_VS/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+void Initialize();
+
+void GetInput();
+void Update();
+void Render();
+
+void Shutdown();
+
 int main()
 {
 	cout << "Hello World" << endl;
